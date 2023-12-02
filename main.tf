f58cc95f9e7d798acdc396768eabc95f3b356647
Emeka Umejiofor <pumej1985@gmail.com>
2023-12-02 00:08:06 -0500

    updated files for provisioning of db

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_db_instance" "pumejrds-instance" {
   parameter_group_name   = "default.mysql5.7"
   db_subnet_group_name   = aws_db_subnet_group.mysql_subnet_group.name
   skip_final_snapshot    = true
-  publicly_accessible    = true
+  publicly_accessible    = false
   vpc_security_group_ids = [aws_security_group.mysql_sg.id]
 
   lifecycle {
